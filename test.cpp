ef9e26447dc9026ccb48e9d542bd0054e7c3d093
清泠 <523912729@qq.com>
2021-05-03 16:25:15 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,38 @@
+/*Changing your GitHub username
+You can change your GitHub username at any time.
+
+In this article
+About username changes
+Repository references
+Links to your previous profile page
+Your Git commits
+Changing your username
+Further reading
+About username changes
+You can change your username to another username that is not currently in use. If the username you want is not available, you'll see information about whether you can request the username to be released when you type in the desired username.
+
+If the username is not eligible for release and you don't hold a trademark for the username, you can choose another username or keep your current username. GitHub Support cannot release the unavailable username for you. For more information, see "Changing your username."
+
+After changing your username, your old username becomes available for anyone else to claim. Most references to your repositories under the old username automatically change to the new username. However, some links to your profile won't automatically redirect.
+
+//abc°¡Å¶zxc
+
+GitHub cannot set up redirects for:
+
+@mentions using your old username
+Links to gists that include your old username
+Repository references
+After you change your username, GitHub will automatically redirect references to your repositories.
+
+Web links to your existing repositories will continue to work. This can take a few minutes to complete after you make the change.
+Command line pushes from your local repository clones to the old remote tracking URLs will continue to work.
+If the new owner of your old username creates a repository with the same name as your repository, that will override the redirect entry and your redirect will stop working. Because of this possibility, we recommend you update all existing remote repository URLs after changing your username. For more information, see "Managing remote repositories."
+
+Links to your previous profile page
+After changing your username, links to your previous profile page, such as https://github.com/previoususername, will return a 404 error. We recommend updating any links to your GitHub account from elsewhere, such as your LinkedIn or Twitter profile.
+
+Your Git commits
+Git commits that were associated with your GitHub-provided noreply email address won't be attributed to your new username and won't appear in your contributions graph. If your Git commits are associated with another email address you've added to your GitHub account, including the ID-based GitHub-provided noreply email address, they'll continue to be attributed to you and appear in your contributions graph after you've changed your username. For more information on setting your email address, see "Setting your commit email address."
+
+Changing your username
+In the upper-right corner of any page, click your profile photo, then click Settings.*/
